eaac4aba0185d7e2247897a80ba02e5157a7945a
owenlevin <owenlevin@gmail.com>
2025-11-28 20:08:24 -0600

    Amplitude bar changes color at thresholds

--- Scenes/Game/AmplitudeBar/AmplitudeBar.gd
+++ Scenes/Game/AmplitudeBar/AmplitudeBar.gd
@@ -1,8 +1,28 @@
-extends TextureProgressBar
+extends Control
 
+@export var healthy_color = Color.GREEN
+@export var caution_color = Color.YELLOW
+@export var danger_color = Color.RED
+
+@export var caution_zone = 0.6
+@export var danger_zone = 0.3
+
+var max_value = 100.0
+@onready var progress = $Progress
 
 func _ready() -> void:
+	set_color()
 	signal_bus.amplitude_changed.connect(update)
 
+
 func update(amplitude) -> void:
-	value = amplitude
+	progress.value = amplitude
+	set_color()
+
+func set_color():
+	if progress.value / max_value >= caution_zone:
+		globals.set_tint(progress, healthy_color, 1)
+	elif progress.value / max_value >= danger_zone:
+		globals.set_tint(progress, caution_color, 1)
+	else:
+		globals.set_tint(progress, danger_color, 1)
